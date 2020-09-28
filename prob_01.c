8f272fc6fe560c1cd66248e7b1857addda29a183
b50d <64529946+b50d@users.noreply.github.com>
2020-09-28 18:31:34 -0500

    Add files via upload

--- prob_01.c
+++ prob_01.c
@@ -1,20 +1,24 @@
+// Code souce from :
+// https://mpitutorial.com/tutorials/mpi-send-and-receive/
+
 #include </usr/local/include/mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-  const int PING_PONG_LIMIT = 10;
+  const int PING_PONG_LIMIT = 11;
 
   MPI_Init(NULL, NULL);
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
+/*
   if (world_size != 2) {
     fprintf(stderr, "World size must be two for %s\n", argv[0]);
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
+*/
 
   int ping_pong_count = 0;
   int partner_rank = (world_rank + 1) % 2;
@@ -27,7 +31,7 @@ int main(int argc, char** argv) {
     } else {
       MPI_Recv(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
                MPI_STATUS_IGNORE);
-      printf("%d received ping_pong_count %d de %d\n",
+      printf("%d recive ping_pong_count %d de %d\n",
              world_rank, ping_pong_count, partner_rank);
     }
   }
